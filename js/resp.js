7b03c7ba572cad613039f37f982c64cc20d2315f
YogeshPanchal <yogeshpanchal28082001@gmail.com>
2023-03-10 19:40:13 +0530

    First Commit

---
+++ js/resp.js
@@ -0,0 +1,10 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navList=document.querySelector('.nav-List')
+rightNav=document.querySelector('.rightNav')
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class-resp');
+    nav-List.classList.toggle('v-class-resp');
+    navbar=document.querySelector('.h-nav-resp');
+
+})
